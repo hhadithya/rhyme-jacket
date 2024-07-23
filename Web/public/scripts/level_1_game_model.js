89357ff9f720ca6249b20ed07daf233b30b0bb6e
Akith-002 <chandinubaa.22@uom.lk>
2024-07-23 11:14:03 +0530

    gyro data added to models

--- Web/public/scripts/level_1_game_model.js
+++ Web/public/scripts/level_1_game_model.js
@@ -3,6 +3,7 @@ import { GLTFLoader } from "jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 const canvas = document.getElementById("level1_model_canvas");
+
 let rightArm, leftArm, model, rightArm2, leftArm2, model2;
 let audioBuffer, audioContext, audioSource;
 let partDuration;
@@ -13,17 +14,61 @@ let targetAngle = 0;
 const music1_url = "../music/level_1_music.mp3"; // Adjust the path as necessary
 
 // sensor data
-let left_gyro_x = 0;
 let left_gyro_y = 0;
-let left_gyro_z = 0;
-let right_gyro_x = 0;
+let left_gyro_p = 0;
+let left_gyro_r = 0;
 let right_gyro_y = 0;
-let right_gyro_z = 0;
-let middle_gyro_x = 0;
+let right_gyro_p = 0;
+let right_gyro_r = 0;
 let middle_gyro_y = 0;
-let middle_gyro_z = 0;
+let middle_gyro_p = 0;
+let middle_gyro_r = 0;
 let right_flex = 0;
 let left_flex = 0;
+
+connection.onmessage = function (event) {
+  event.data.text().then((text) => {
+    const data = JSON.parse(text);
+    console.log(data);
+
+    if (data.flex1Angle !== undefined) {
+      left_flex = data.flex1Angle;
+    }
+    if (data.flex2Angle !== undefined) {
+      right_flex = data.flex2Angle;
+    }
+
+    if (data.yawRight !== undefined) {
+      right_gyro_y = data.yawRight;
+    }
+    if (data.pitchRight !== undefined) {
+      right_gyro_p = data.pitchRight;
+    }
+    if (data.rollRight !== undefined) {
+      right_gyro_r = data.rollRight;
+    }
+
+    if (data.yawLeft !== undefined) {
+      left_gyro_y = data.yawLeft;
+    }
+    if (data.pitchLeft !== undefined) {
+      left_gyro_p = data.pitchLeft;
+    }
+    if (data.rollLeft !== undefined) {
+      left_gyro_r = data.rollLeft;
+    }
+
+    if (data.yawCenter !== undefined) {
+      middle_gyro_y = data.yawCenter;
+    }
+    if (data.pitchCenter !== undefined) {
+      middle_gyro_p = data.pitchCenter;
+    }
+    if (data.rollCenter !== undefined) {
+      middle_gyro_r = data.rollCenter;
+    }
+  });
+};
 
 async function loadAudio() {
   if (!music1_url) return;
@@ -115,13 +160,13 @@ function init() {
 
         // define the correct conditions for the following if statements
 
-        // if (gyro_value){
-        //   if(isRaised && rightArm.rotation.x < 0.9){
-        //     targetAngle += (gyro_value-targetAngle);
+        // if (left_gyro_p, right_gyro_p) {
+        //   if (isRaised && rightArm.rotation.x < 0.9) {
+        //     targetAngle += gyro_value - targetAngle;
         //   }
 
-        //   if(!isRaised && rightArm.rotation.x >-0.9){
-        //     targetAngle -= (gyro_value-targetAngle);
+        //   if (!isRaised && rightArm.rotation.x > -0.9) {
+        //     targetAngle -= gyro_value - targetAngle;
         //   }
         // }
 
@@ -186,6 +231,7 @@ function init() {
             leftArm2.rotation.x = rightArm2.rotation.x;
           }
         }
+
         renderer.render(scene, camera);
       }
 
@@ -212,37 +258,6 @@ function init() {
   floor.position.y = -11;
   scene.add(floor);
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const socket = new WebSocket("ws://" + window.location.host);
-
-    socket.addEventListener("message", (event) => {
-      event.data.text().then((text) => {
-        const data = JSON.parse(text);
-        // console.log(data);
-        // // assign data to the global variables
-        // l_gyro_x = data.rollAngle;
-        // l_gyro_y =
-        // l_gyro_z =
-        // r_gyro_x =
-        // r_gyro_y =
-        // r_gyro_z =
-        // m_gyro_x =
-        // m_gyro_y =
-        // m_gyro_z =
-        // r_flex =
-        // l_flex =
-      });
-    });
-
-    socket.onopen = () => {
-      console.log("WebSocket connection opened");
-    };
-
-    socket.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
-  });
-
   // -----------don't delete------------------------
 
   function checkAndPlayNextPart(newIsRaised) {
@@ -252,17 +267,19 @@ function init() {
     }
   }
 
-  // if (gyro_x > 50) {
-  //   isRaised = true; // Toggle the state
-  //   checkAndPlayNextPart(isRaised);
-  //   targetAngle = 0.95; // Set the target angle based on the state
-  // }
+  setInterval(() => {
+    if (right_gyro_p > 50 && left_gyro_p > 50) {
+      isRaised = true; // Toggle the state
+      checkAndPlayNextPart(isRaised);
+      targetAngle = 0.95; // Set the target angle based on the state
+    }
 
-  // if(gyro_x >){
-  // isRaised = false; // Toggle the state
-  // checkAndPlayNextPart(isRaised);
-  // targetAngle = -0.95; // Set the target angle based on the state
-  // }
+    if (right_gyro_p < -50 && left_gyro_p < -50) {
+      isRaised = false; // Toggle the state
+      checkAndPlayNextPart(isRaised);
+      targetAngle = -0.95; // Set the target angle based on the state
+    }
+  }, 100);
 
   // --------------------------------------------------
 }
